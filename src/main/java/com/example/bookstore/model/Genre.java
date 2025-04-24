7058334c46ccfa09518a1bc19bf3909f8e1ba251
Ariana <ariana.toncean09@yahoo.com>
2025-04-24 10:55:06 +0300

    Initial commi

---
+++ src/main/java/com/example/bookstore/model/Genre.java
@@ -0,0 +1,21 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
